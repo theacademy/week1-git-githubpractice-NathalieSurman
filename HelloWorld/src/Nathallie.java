6061f60ae3380207e4899bc14eb7dc12fc865083
Nathalie Surman <onepiecenat@hotmail.com>
2023-05-09 14:34:04 -0400

    Code along in IntellJ

---
+++ HelloWorld/src/Nathallie.java
@@ -0,0 +1,10 @@
+public class Nathallie {
+    public static void main(String[] args){
+
+                System.out.println("  *  \n * * \n*   *\n * * \n  *  \n");
+
+                System.out.println("This is for the Code Along in IntelliJ");
+
+
+    }
+}
